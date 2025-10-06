476c98cfd16790213a4a2cc0eb7f1b648fca9389
data4decision international <data4decision25@gmail.com>
2025-10-06 07:13:34 -0700

    update

--- src/app/lib/mongo.ts
+++ src/app/lib/mongo.ts
@@ -1,16 +1,4 @@
-// import { MongoClient, Db } from "mongodb";
 
-// const uri = process.env.MONGO_URI!;
-// let client: MongoClient | null = null;
-// let db: Db | null = null;
-
-// export async function getDb(): Promise<Db> {
-//   if (db) return db;
-//   if (!client) client = new MongoClient(uri);
-//   if (!client.topology?.isConnected()) await client.connect();
-//   db = client.db(); // default DB from URI
-//   return db;
-// }
 import { MongoClient, Db } from "mongodb";
 
 const uri = process.env.MONGO_URI!;
