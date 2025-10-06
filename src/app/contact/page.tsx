a72b20dd514b3229faf407244a6dea015a1184ca
data4decision international <data4decision25@gmail.com>
2025-10-06 07:27:12 -0700

    update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Navbar from "../components/landing/Navbar";
-
+import Navbar from "@/components/landing/Navbar";
+import Footer from "@/components/landing/Footer"
 const page = () => {
   return (
     <div>
       <Navbar />
       Contact
+      <Footer/>
     </div>
   );
 };
